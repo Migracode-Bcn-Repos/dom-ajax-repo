2ca04fcf085b1f4d1f0742a2b1348a2cfcce0479
Leander Rysanek <leander@hosco.com>
2020-05-16 13:27:57 +0200

    feat: working version

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,54 @@
-// Write code here to communicate with Github
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const URL = "https://api.github.com/users/leandroviajando/repos";
+const REPOS_LIST_ID = "repos-list";
+const REPOS_COUNT_ID = "repos-count";
+const FILTER_BY_USER_FORM_ID = "filter-repos-by-user";
+const FILTER_BY_USER_INPUT_ID = "filter-repos-by-user-input";
+
+const reposListElement = document.getElementById(REPOS_LIST_ID);
+reposListElement.innerHTML = null;
+
+const reposCountElement = document.getElementById(REPOS_COUNT_ID);
+
+updatePage();
+
+const reposFilterFormElement = document.getElementById(FILTER_BY_USER_FORM_ID);
+const reposFilterInputElement = document.getElementById(
+  FILTER_BY_USER_INPUT_ID
+);
+reposFilterFormElement.addEventListener("submit", event => {
+  event.preventDefault();
+  updatePage(reposFilterInputElement.value);
+  reposFilterInputElement.value = "";
+});
+
+function updatePage(userId) {
+  reposListElement.innerHTML = null;
+  fetch(
+    PROXY_URL + (userId ? `https://api.github.com/users/${userId}/repos` : URL)
+  )
+    .then(response => response.json())
+    .then(repos => {
+      updateList(repos);
+      updateCount(repos);
+    });
+}
+
+function updateList(repos) {
+  repos.forEach(repo => {
+    const listItemLinkElement = document.createElement("a");
+    listItemLinkElement.appendChild(document.createTextNode(repo.name));
+    listItemLinkElement.setAttribute("href", repo.html_url);
+
+    const listItemElement = document.createElement("li");
+    listItemElement.appendChild(listItemLinkElement);
+
+    reposListElement.appendChild(listItemElement);
+  });
+}
+
+function updateCount(repos) {
+  reposCountElement.innerText = repos.length;
+  repos.length &&
+    reposCountElement.setAttribute("href", repos[0].owner.html_url);
+}
---
+++ js/pull-requests.js
@@ -0,0 +1,35 @@
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const URL = "https://api.github.com/repos/codeyourfuture/js-exercises/pulls";
+const PRS_LIST_ID = "pull-requests-list";
+const FILTER_BY_USER_ID = "filter-prs-by-user";
+
+const prsListElement = document.getElementById(PRS_LIST_ID);
+prsListElement.style.listStyle = "disc inside none";
+prsListElement.innerHTML = null;
+
+const filterByUserInputElement = document.getElementById(FILTER_BY_USER_ID);
+
+fetch(PROXY_URL + URL)
+  .then(response => response.json())
+  .then(prs => {
+    updateList(prs);
+    filterByUserInputElement.addEventListener("keyup", event =>
+      updateList(prs, event.target.value)
+    );
+  });
+
+function updateList(prs, searchTerm) {
+  prsListElement.innerHTML = null;
+  prs
+    .filter(pr => (searchTerm ? pr.user.login.includes(searchTerm) : true))
+    .forEach(pr => {
+      const listItemLinkElement = document.createElement("a");
+      listItemLinkElement.appendChild(document.createTextNode(pr.title));
+      listItemLinkElement.setAttribute("href", pr.html_url);
+
+      const listItemElement = document.createElement("li");
+      listItemElement.appendChild(listItemLinkElement);
+
+      prsListElement.appendChild(listItemElement);
+    });
+}
